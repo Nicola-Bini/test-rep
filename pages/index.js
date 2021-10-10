ec8688ea3cf870bfb9b60181c9f7a3a3ebd90ef9
Nicola Bini <63654846+Nicola-Bini@users.noreply.github.com>
2021-10-09 22:29:01 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image src="dashboard" />
+        <Image src="dashboard" alt="dashboard"/>
       </main>
     </div>
   )
